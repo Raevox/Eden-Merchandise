a9876a3888611a512fada9d03e31e0c2cfef4e48
Manpreet "Mannie" Singh <raevox@gmail.com>
2016-10-12 19:17:30 -0400

    Implement shopping cart toggle

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,6 +4,14 @@ require('bootstrap');
 
 const $ = global.jQuery;
 
-$('.flip').click(event => {
+function toggleProductFlip(event) {
   $(event.currentTarget).parents('.product').toggleClass('flipped');
-});
+}
+
+function toggleShoppingCart() {
+  $('#shopping-cart').toggleClass('open');
+}
+
+$('.flip').click(toggleProductFlip);
+
+$('#shopping-cart-toggle').click(toggleShoppingCart);
