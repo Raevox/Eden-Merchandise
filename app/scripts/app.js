07ad8e2126eca295fd8e8c8e033e9b237cc3328d
Manpreet "Mannie" Singh <raevox@gmail.com>
2016-10-12 21:22:29 -0400

    Implement most of the shopping cart functions -- just missing display

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,6 +14,11 @@ function toggleShoppingCart() {
   $('#shopping-cart').toggleClass('open');
 }
 
+function handleAddToCart(event) {
+  const product = $(event.currentTarget).parents('.product');
+  addToCart(product);
+}
+
 function readShoppingCart() {
   const localCart = JSON.parse(localStorage.getItem('shoppingCart'));
 
@@ -37,9 +42,52 @@ function writeShoppingCart() {
   return shoppingCartJSON;
 }
 
+function buildProductObj(product) {
+  const price = parseInt(
+    product.find('.information > .price')
+      .html()
+      .substring(1)
+  );
+
+  const color = product.find('.color > span').css('backgroundColor');
+
+  return {
+    price,
+    color,
+    quantity: 1,
+    timestamp: new Date()
+  };
+}
+
+function addToCart(product) {
+  const name = product.find('.information > .name').html();
+
+  if (shoppingCart[name]) {
+    shoppingCart[name].quantity++;
+  } else {
+    product.addClass('carted');
+    shoppingCart[name] = buildProductObj(product);
+  }
+
+  return shoppingCart[name];
+}
+
+function removeFromCart(name) {
+  const product = shoppingCart[name];
+
+  if (product.quantity == 1) {
+    delete shoppingCart[name];
+    return 0;
+  }
+
+  product.quantity--;
+  return product.quantity;
+}
+
 function init() {
   $('.flip').click(toggleProductFlip);
   $('#shopping-cart-toggle').click(toggleShoppingCart);
+  $('.action > .fa-cart-plus').click(handleAddToCart);
 
   readShoppingCart();
 }
