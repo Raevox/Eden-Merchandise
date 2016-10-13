a16e574ed417d128265a032116254df49270da4c
Manpreet "Mannie" Singh <raevox@gmail.com>
2016-10-12 20:38:00 -0400

    Implement localStorage persistency to shopping cart

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,6 +4,8 @@ require('bootstrap');
 
 const $ = global.jQuery;
 
+const shoppingCart = {};
+
 function toggleProductFlip(event) {
   $(event.currentTarget).parents('.product').toggleClass('flipped');
 }
@@ -12,9 +14,34 @@ function toggleShoppingCart() {
   $('#shopping-cart').toggleClass('open');
 }
 
+function readShoppingCart() {
+  const localCart = JSON.parse(localStorage.getItem('shoppingCart'));
+
+  if (localCart && !$.isEmptyObject(localCart)) {
+    localCart.forEach(product => {
+      shoppingCart.push(product);
+    });
+  }
+
+  return shoppingCart;
+}
+
+function writeShoppingCart() {
+  if (!shoppingCart || !$.isEmptyObject(shoppingCart)) {
+    localStorage.removeItem('shoppingCart');
+    return '[]';
+  }
+
+  const shoppingCartJSON = JSON.stringify(shoppingCart);
+  localStorage.setItem('shoppingCart', shoppingCartJSON);
+  return shoppingCartJSON;
+}
+
 function init() {
   $('.flip').click(toggleProductFlip);
   $('#shopping-cart-toggle').click(toggleShoppingCart);
+
+  readShoppingCart();
 }
 
 $(document).ready(init);
